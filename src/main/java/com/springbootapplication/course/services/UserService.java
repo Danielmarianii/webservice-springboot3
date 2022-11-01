512b8c934da7c8e1de06870a4f7ffa02d0e60fb1
“danielmarianii” <danielmariani10@hotmail.com>
2022-11-01 14:50:10 -0300

    Exception handling - update

--- src/main/java/com/springbootapplication/course/services/UserService.java
+++ src/main/java/com/springbootapplication/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.springbootapplication.course.repositories.UserRepository;
 import com.springbootapplication.course.services.exceptions.DatabaseException;
 import com.springbootapplication.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -46,9 +48,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 
